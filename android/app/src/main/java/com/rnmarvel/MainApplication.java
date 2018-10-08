25a9cac231d15e52dbd07eefde7da5f3f056d147
Sergio Cabrera Hernández <sergio.cabrera.hernandez@gmail.com>
2018-10-09 00:50:32 +0100

    Rename App to rnmarvel.

--- android/app/src/main/java/com/rnmarvel/MainApplication.java
+++ android/app/src/main/java/com/rnmarvel/MainApplication.java
@@ -3,13 +3,12 @@ package com.rnmarvel;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.react.rnspinkit.RNSpinkitPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.imagepicker.ImagePickerPackage; // IMAGE PICKER
-import com.react.rnspinkit.RNSpinkitPackage; // SPINKIT
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +23,11 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(new ImagePickerPackage(), new RNSpinkitPackage(), new MainReactPackage());
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNSpinkitPackage(),
+            new ImagePickerPackage()
+      );
     }
 
     @Override
